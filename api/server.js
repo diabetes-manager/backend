7a6c609bc99fe1c2f049fb91a29f54676cc35a65
Frank Martinez <frankie.martinez@gmail.com>
2019-04-18 09:38:54 -0700

    removed tests, mostly reverted to last known working

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
-// const path = require('path');
 
 const configureRoutes = require('./routes/router.js');
 
